97ea50859053f1020cad22fcca4c33f19dde93ab
Sergio99 <sergionunesricardonunes@gmail.com>
2024-04-20 17:30:27 +0100

    initial commit

---
+++ src/main/java/com/tui/proof/service/pilote/PiloteManageImpl.java
@@ -0,0 +1,23 @@
+package com.tui.proof.service.pilote;
+
+import com.tui.proof.dto.pilote.OrderDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PiloteManageImpl implements PiloteManager {
+
+    @Override
+    public void createOrder(OrderDto newOrderDto) {
+        
+    }
+
+    @Override
+    public void updateOrder(OrderDto order) {
+
+    }
+
+    @Override
+    public void listOrders(String filter) {
+
+    }
+}
